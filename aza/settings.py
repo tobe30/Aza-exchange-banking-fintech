4dbca4a48088af4e4ecee80f268b3d24555630d9
tobe <tobemarizu@gmail.com>
2025-04-02 15:10:03 +0100

    added whitenoise

--- aza/settings.py
+++ aza/settings.py
@@ -131,8 +131,8 @@ LOGOUT_REDIRECT_URL = "userauths:login"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_SKIP_MISSING_FILES = True
 
 
 MEDIA_URL ='/media/'
